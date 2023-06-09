32e87d66533cf5cc0c8a65374255b971b4642444
Sravani Ratna Yeddu <112679917+SravaniRatna@users.noreply.github.com>
2023-06-09 17:20:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
  const items = document.querySelectorAll('.item');
+const container1 = document.getElementById('container1');
     const container2 = document.getElementById('container2');
     const successMessage = document.getElementById('successMessage');
 
@@ -44,15 +45,20 @@
       this.appendChild(draggedItem);
       successMessage.style.display = 'block';
       successMessage.innerText = 'Item dropped successfully!';
+     
+     // Hide the success message after 2 seconds (2000 milliseconds)
+  setTimeout(function() {
+    successMessage.style.display = 'none';
+  }, 2000);
     }
 
 function reset() {
       container2.innerHTML = '';
       successMessage.style.display = 'none';
       container1.innerHTML = `
-        <div class="item" draggable="true">Item 1</div>
-        <div class="item" draggable="true">Item 2</div>
-        <div class="item" draggable="true">Item 3</div>
+        <div class="item" draggable="true">Hey!</div>
+        <div class="item" draggable="true">Hello</div>
+        <div class="item" draggable="true"><img src="C:\Users\srava\Downloads\penguine.avif" alt="Image"></div>
       `;
 
       items.forEach(item => {
